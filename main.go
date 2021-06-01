3cba04ccb0a83e9b3482e679c77361df71f97231
Pholawat Tangsatit <pholawat.tangsatit@hotmail.com>
2021-06-01 20:27:48 +0700

    Implemented Simple Downloading
    A single-threaded downloading function is implemented and can be used with the command "download".
    Sample Usage : concurrent-downloader download https://mycourseville-default.s3.ap-southeast-1.amazonaws.com/course_files/2020_2/23061/materials/Syllabus_2190462_2020.7381.1610930392.2847.pdf

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"strconv"
 )
 
 var commandList = map[string]func(params []string){
-	"help": helpCommand,
-	"test": testCommand,
+	"help":     helpCommand,
+	"test":     testCommand,
 	"download": downloadCommand,
 }
 
@@ -22,71 +24,69 @@ var helpList = map[string]string{
 
 var helpText = "\nUsage: concurrent-downloader COMMAND [PARAMETERS]\n\nCommands:\n"
 
-
-
-func main(){
-	for key , val := range helpList {
-		helpText += fmt.Sprintf("	%s	%s\n",key,val)
+func main() {
+	for key, val := range helpList {
+		helpText += fmt.Sprintf("	%s	%s\n", key, val)
 	}
 	hasArgs, args := parseArgs()
-	if hasArgs{
+	if hasArgs {
 		parseCommand(args)
-	}else{
+	} else {
 		fmt.Print(helpText)
 	}
 }
 
-func parseArgs() (bool,[]string) {
-	if(len(os.Args) > 1) {
+func parseArgs() (bool, []string) {
+	if len(os.Args) > 1 {
 		return true, os.Args[1:]
 	}
-	
-	return false,[]string{}
+
+	return false, []string{}
 }
 
 func parseCommand(args []string) {
 	var command string
 	var params []string
-	if(len(args) > 1){
+	if len(args) > 1 {
 		command = args[0]
 		params = args[1:]
-	}else{
+	} else {
 		command = args[0]
 		params = make([]string, 0)
 	}
-	
-	if val, ok := commandList[command]; ok{
+
+	if val, ok := commandList[command]; ok {
 		val(params)
 		return
 	}
 	fmt.Printf("The command %s doesn't exist\n", command)
 }
 
-func helpCommand(params []string){
+func helpCommand(params []string) {
 	if len(params) > 1 {
 		fmt.Println("Invalid Input : This command expects no parameter!")
 	} else if len(params) == 1 {
 		if val, ok := helpList[params[0]]; ok {
 			fmt.Println(val)
-		}else{
+		} else {
 			fmt.Printf("The command %s doesn't exist!\n", params[0])
 		}
-	} else{
+	} else {
 		fmt.Print(helpText)
 	}
 }
 
-func testCommand(params []string){
+func testCommand(params []string) {
 	if len(params) != 1 {
 		fmt.Println("Invalid Input : This command expects 1 parameter, which is the file's URL")
-	}else{
+	} else {
 		res, err := http.Head(params[0])
 		if err != nil {
-   			log.Fatal(err)
+			log.Fatal(err)
 		}
 		if res.StatusCode == http.StatusOK && res.Header.Get("Accept-Ranges") == "bytes" {
 			fmt.Printf("%s can be downloaded with concurrent-downloader\n", params[0])
-		}else{
+		} else {
 			fmt.Printf("%s can't be downloaded with concurrent-downloader\n", params[0])
 		}
 	}
@@ -95,7 +95,7 @@ func testCommand(params []string){
 func downloadCommand(params []string) {
 	if len(params) != 1 {
 		fmt.Println("Invalid Input : This command expects 1 parameter, which is the file's URL")
-	}else{
+	} else {
 		res, err := http.Head(params[0])
 		if err != nil {
 			log.Fatal(err)
@@ -106,13 +106,35 @@ func downloadCommand(params []string) {
 			if err != nil {
 				log.Fatal(err)
 			}
-			fmt.Println("Start Concurrent Download...")
+			simpleDownload(params[0])
 			_ = contentSize
 		} else {
-			fmt.Println("Start Simple Download...")
+			simpleDownload(params[0])
 		}
 	}
-	
+
 }
 
+func simpleDownload(url string) {
 
+	// make a request
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	// create the output file
+	f, err := os.OpenFile(path.Base(url), os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// copy to output file
+	buffer := make([]byte, 1024)
+	_, err = io.CopyBuffer(io.MultiWriter(f), res.Body, buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
